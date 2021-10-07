94bb6c94dcecd25aee1a88835fc78bd01f7f7a66
santdv <viva.santiago360@gmail.com>
2021-10-07 18:54:32 -0300

    darkmode add and some correction in cv

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+
 $(document).ready(function(){
+
+    $('#toggle').click(function(){
+        
+        $('#toggle').toggleClass('active');
+        $('body').toggleClass('active');
+        $('header').toggleClass('active');
+        
+    })
+
 
     $('#menu').click(function(){
 
@@ -12,18 +22,27 @@ $(document).ready(function(){
         $(this).removeClass('fa-times');
         $('header').removeClass('toggle');
 
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
+        }
+
     })
 
-    $('a[href*="#"]').on('click', function(e){
+    $('a[href*="#"]').on('click',function(e){
 
         e.preventDefault();
-
+    
         $('html, body').animate({
-
-            scrollTop : $($(this).attr('href')).offset().top,
-
-            }, 500, 'linear'
+    
+          scrollTop : $($(this).attr('href')).offset().top,
+    
+        },
+          500, 
+          'linear'
         );
-
+    
+      });
+    
     });
-});
